4b7476479bcf79cf33e1d82716d9b0746f67e758
Alert175 <ilya290897@gmail.com>
2021-09-24 00:48:05 +0300

    backup

--- src/domains/image/controllers/image.controller.ts
+++ src/domains/image/controllers/image.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { ImageService } from '../services/image.service';
+import { Controller, Get, Query, HttpStatus, Res } from "@nestjs/common";
+import { ImageService } from "../services/image.service";
 
 @Controller("/optimize")
 export class ImageController {
-  constructor(private readonly imageService: ImageService) {}
+  constructor(private readonly imageService: ImageService) {
+  }
 
   @Get()
-  getHello(): string {
-    return this.imageService.getHello();
+  async getHello(@Query() query, @Res() res): Promise<string> {
+    const {url, h: heightImage, w: widthImage} = query;
+    if (!url) {
+      res.status(404).send('not found');
+      return;
+    }
+    const resultImage = await this.imageService.getOptimizeImage(url, heightImage || '', widthImage || '');
+    res.status(200).sendFile(resultImage);
   }
 }
--- src/domains/image/services/image.service.ts
+++ src/domains/image/services/image.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from "@nestjs/common";
+import { sharp } from "sharp";
+import axios from "axios";
 
+/**
+ * Сервис работы с изображением
+ */
 @Injectable()
 export class ImageService {
-  getHello(): string {
-    return "Hello World!";
+  async getOptimizeImage(url: string, height: string, width: string): Promise<object> {
+    try {
+      if (!url) {
+        return null;
+      }
+      const responseImage = await axios.get(url, { responseType: "arraybuffer" });
+      const { data } = responseImage;
+      console.log(data);
+      return data;
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
   }
 }
